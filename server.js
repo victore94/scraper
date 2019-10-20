bebe58cfaa2bfee18bb7791e72846db1e9aaec58
Victor Lopez <victorel94@gmail.com>
2019-10-20 00:55:36 -0700

    finished

--- server.js
+++ server.js
@@ -32,8 +32,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
 
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
+
+mongoose.connect(MONGODB_URI)
 
 ///////////////////////////////////
 
