f87014372843085446021745fca917178cb6e10f
Victor Lopez <victorel94@gmail.com>
2019-10-26 16:47:15 -0700

    working

--- server.js
+++ server.js
@@ -1,71 +1,26 @@
 
-// var express = require("express");
-// var mongojs = require("mongojs");
-// var axios = require("axios");
-// var cheerio = require("cheerio");
-// var logger = require("morgan");
-// var mongoose = require("mongoose");
-
-
-
-// var PORT = process.env.PORT || 3000;
-// var app = express();
-
-
-
-// var databaseUrl = "scraper";
-// var collections = ["scrapeAnime"];
-// /////////////////////////////////
-
-
-
-
-
-// app.use(logger("dev"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static("public"));
-
-
-// var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
-
-// mongoose.connect(MONGODB_URI)
-
-
-// var db = mongojs(databaseUrl, collections);
-///////////////////////////////////
-
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
 
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
+
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
+
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 
